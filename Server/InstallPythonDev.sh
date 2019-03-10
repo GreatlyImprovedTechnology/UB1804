d57b13b8455a0c54847c19eaff62d804cbf6eeed
John.ORaw <john.oraw@iotech.ie>
2019-03-10 16:49:03 +0000

    Directory Structure

---
+++ Server/InstallPythonDev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# by: JOR
+# Date: 11DEC18
+# Function: Configure network settings in UB1804
+# Script: UB1804PythonDev.sh
+
+sudo apt install -y python-pip
+sudo apt install python-dev
+sudo apt install -y python3-venv

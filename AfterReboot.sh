a98d4c307efae482810e023de267562a38d93889
JOR <46943375+GreatlyImprovedTechnology@users.noreply.github.com>
2019-03-13 17:26:13 +0000

    Update AfterReboot.sh

--- AfterReboot.sh
+++ AfterReboot.sh
@@ -15,8 +15,9 @@ echo "Checking for existing keys"
 ls -al ~/.ssh
 
 # Start the ssh agent
-ssh-agent $SHELL
+eval "$(ssh-agent -s)"
 
 # Add keys to the SSH agent
-ssh-add ~/.ssh/JOR.rsa
+ssh-add ~/.ssh/git.rsa
+ssh-add ~/.ssh/local.rsa
 cd

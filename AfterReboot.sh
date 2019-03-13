a60af5de77973a9ea33fae1dab465b3044e91469
JOR <46943375+GreatlyImprovedTechnology@users.noreply.github.com>
2019-03-13 15:36:47 +0000

    Create AfterReboot.sh

---
+++ AfterReboot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# by: JOR Date: 13MAR19 Function: Set up GIT for SSH Script: SetUpSSH.sh
+clear
+echo "***********************************************"
+echo "Welcome " $USER
+echo "This script start the SSH agent and loads"
+echo "the keys"
+echo "Edit before use!!"
+echo "***********************************************"
+echo "Press return to continue, [ctrl][c] to exit"
+read
+
+# Check for existing keys
+echo "Checking for existing keys"
+ls -al ~/.ssh
+
+# Start the ssh agent
+ssh-agent $SHELL
+
+# Add keys to the SSH agent
+ssh-add ~/.ssh/JOR.rsa
+cd

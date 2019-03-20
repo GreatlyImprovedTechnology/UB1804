778f26ed83031b4f472a7085e606722879c887e0
John.ORaw <john.oraw@iotech.ie>
2019-03-20 19:05:31 +0000

    Ansible Install script

---
+++ Server/AnsibleServer/InstallAnsible.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# by: JOR
+# Date: 20MAR19
+# Function: install Ansible in UB18.04.02
+# Script: InstallAnsible.sh
+
+echo "****************************************************"
+echo "Welcome" $USER
+echo "This script will do updates and then install" 
+echo "Ansible in UB1804"
+echo "****************************************************"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install ansible -y
+
+

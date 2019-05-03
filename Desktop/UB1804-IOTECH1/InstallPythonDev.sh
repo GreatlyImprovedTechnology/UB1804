f63eed1c8d6efbb21ce7c3b66a7a5dd80e60a5aa
JOR <46943375+GreatlyImprovedTechnology@users.noreply.github.com>
2019-05-03 19:13:31 +0100

    Update InstallPythonDev.sh

--- Desktop/UB1804-IOTECH1/InstallPythonDev.sh
+++ Desktop/UB1804-IOTECH1/InstallPythonDev.sh
@@ -4,6 +4,9 @@
 # Function: Configure network settings in UB1804
 # Script: UB1804PythonDev.sh
 
-sudo apt install -y python-pip
-sudo apt install python-dev
+#sudo apt install -y python-pip
+sudo apt install -y python3-pip
+sudo apt install -y python3-dev
+#sudo apt install python-dev
 sudo apt install -y python3-venv
+pip3 install mysql-connector-python
